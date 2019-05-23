fb5d4762082552365e88947974004ba29b40214a
qw <ikherty@gmail.com>
2019-05-23 19:53:41 +0300

    images add

--- OPD/menu.cpp
+++ OPD/menu.cpp
@@ -1,11 +1,16 @@
 #include "menu.h"
 #include "ui_menu.h"
 #include "pathfile.h"
+#include <QPixmap>
 menu::menu(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::menu)
 {
     ui->setupUi(this);
+    QPixmap pix(":/rec/resourses/3.png");
+    int w=ui->image->width();
+    int h=ui->image->height();
+    ui->image->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
 }
 
 menu::~menu()
--- build-opd-Desktop-Debug/ui_menu.h
+++ build-opd-Desktop-Debug/ui_menu.h
@@ -14,33 +14,71 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QDialog>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
 
 class Ui_menu
 {
 public:
+    QWidget *widget;
+    QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout;
     QLabel *label;
     QPushButton *pushButton;
     QLabel *label_2;
+    QLabel *image;
 
     void setupUi(QDialog *menu)
     {
         if (menu->objectName().isEmpty())
             menu->setObjectName(QStringLiteral("menu"));
-        menu->resize(400, 163);
-        label = new QLabel(menu);
+        menu->resize(466, 173);
+        widget = new QWidget(menu);
+        widget->setObjectName(QStringLiteral("widget"));
+        widget->setGeometry(QRect(30, 20, 403, 130));
+        horizontalLayout = new QHBoxLayout(widget);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        label = new QLabel(widget);
         label->setObjectName(QStringLiteral("label"));
-        label->setGeometry(QRect(80, 30, 251, 16));
-        pushButton = new QPushButton(menu);
+        QFont font;
+        font.setFamily(QStringLiteral("Noto Nastaliq Urdu"));
+        font.setPointSize(12);
+        label->setFont(font);
+
+        verticalLayout->addWidget(label);
+
+        pushButton = new QPushButton(widget);
         pushButton->setObjectName(QStringLiteral("pushButton"));
-        pushButton->setGeometry(QRect(130, 70, 131, 31));
-        label_2 = new QLabel(menu);
+
+        verticalLayout->addWidget(pushButton);
+
+        label_2 = new QLabel(widget);
         label_2->setObjectName(QStringLiteral("label_2"));
-        label_2->setGeometry(QRect(60, 130, 291, 21));
+        QFont font1;
+        font1.setFamily(QStringLiteral("Norasi"));
+        font1.setPointSize(10);
+        font1.setItalic(true);
+        label_2->setFont(font1);
+
+        verticalLayout->addWidget(label_2);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        image = new QLabel(widget);
+        image->setObjectName(QStringLiteral("image"));
+
+        horizontalLayout->addWidget(image);
+
 
         retranslateUi(menu);
 
@@ -53,6 +91,7 @@ public:
         label->setText(QApplication::translate("menu", "\320\230\320\275\321\202\320\265\321\200\320\275\320\265\321\202 \320\277\321\200\320\276\320\262\320\260\320\271\320\264\320\265\321\200 SwiftNet", Q_NULLPTR));
         pushButton->setText(QApplication::translate("menu", "\320\222\321\213\320\261\321\200\320\260\321\202\321\214 \321\204\320\260\320\271\320\273", Q_NULLPTR));
         label_2->setText(QApplication::translate("menu", "\320\224\320\273\321\217 \320\275\320\260\321\207\320\260\320\273\320\260 \321\200\320\260\320\261\320\276\321\202\321\213 \320\262\321\213\320\261\320\265\321\200\320\270\321\202\320\265 \321\204\320\260\320\271\320\273", Q_NULLPTR));
+        image->setText(QString());
     } // retranslateUi
 
 };
